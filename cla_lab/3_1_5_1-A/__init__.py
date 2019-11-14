11f05d075e26fcec947c52a6d5f12b04bbdbcfb0
Carelbos <56919622+Carelbos@users.noreply.github.com>
2019-11-14 22:11:35 +0200

    Create __init__.py

---
+++ cla_lab/3_1_5_1-A/__init__.py
@@ -0,0 +1,17 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """3_1_5_1-A.c exists"""
+    check50.exists("3_1_5_1-A.c")
+    
+@check50.check(exists)
+def compiles():
+    """3_1_5_1-A.c compiles"""
+    check50.c.compile("3_1_5_1-A.c")
+    
+@check50.check(compiles)
+def test1():
+    """First condition is true\nSecond condition is false\nThird condition is true"""
+    check50.run("./3_1_5_1-A").stdout("First condition is true\nSecond condition is false\nThird condition is true", regex=True).exit(0)
