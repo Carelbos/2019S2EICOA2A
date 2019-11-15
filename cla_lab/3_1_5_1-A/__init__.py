748cc650bb2621e7d8881272a7268b65bb740317
Carelbos <56919622+Carelbos@users.noreply.github.com>
2019-11-15 12:51:54 +0200

    Update __init__.py

--- cla_lab/3_1_5_1-A/__init__.py
+++ cla_lab/3_1_5_1-A/__init__.py
@@ -4,14 +4,14 @@ import check50.c
 @check50.check()
 def exists():
     """3_1_5_1-A.c exists"""
-    check50.exists("3_1_5_1-A.c")
+    check50.exists("cla_lab_3_1_5_1-A.c")
     
 @check50.check(exists)
 def compiles():
     """3_1_5_1-A.c compiles"""
-    check50.c.compile("3_1_5_1-A.c")
+    check50.c.compile("cla_lab_3_1_5_1-A.c")
     
 @check50.check(compiles)
 def test1():
     """First condition is true\nSecond condition is false\nThird condition is true"""
-    check50.run("./3_1_5_1-A").stdout("First condition is true\nSecond condition is false\nThird condition is true", regex=True).exit(0)
+    check50.run("./cla_lab_3_1_5_1-A").stdout("First condition is true\nSecond condition is false\nThird condition is true", regex=True).exit(0)
