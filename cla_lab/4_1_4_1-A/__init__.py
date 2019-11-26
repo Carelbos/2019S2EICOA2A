392134dad98ba80ee583d0b0d31375762bbd35a4
Carelbos <56919622+Carelbos@users.noreply.github.com>
2019-11-26 11:03:12 +0200

    Update __init__.py

--- cla_lab/4_1_4_1-A/__init__.py
+++ cla_lab/4_1_4_1-A/__init__.py
@@ -14,22 +14,22 @@ def compiles():
 @check50.check(compiles)
 def test1():
     """ 1 outputs January"""
-    check50.run("./cla_lab_4_1_4_1-A").stdin("1").stdout("January").exit(0)
+    check50.run("./cla_lab_4_1_4_1-A").stdin("1").stdout("January[\n]").exit(0)
 
 @check50.check(compiles)
 def test2():
     """ 6 outputs June"""
-    check50.run("./cla_lab_4_1_4_1-A").stdin("6").stdout("June").exit(0)
+    check50.run("./cla_lab_4_1_4_1-A").stdin("6").stdout("June[\n]").exit(0)
     
 @check50.check(compiles)
 def test3():
     """ 12 outputs December"""
-    check50.run("./cla_lab_4_1_4_1-A").stdin("12").stdout("December").exit(0)
+    check50.run("./cla_lab_4_1_4_1-A").stdin("12").stdout("December[\n]").exit(0)
     
 @check50.check(compiles)
 def test4():
     """ -1 outputs Error: no such month in my calendar"""
-    check50.run("./cla_lab_4_1_4_1-A").stdin("-1").stdout("Error: no such month in my calendar").exit(0)
+    check50.run("./cla_lab_4_1_4_1-A").stdin("-1").stdout("Error: no such month in my calendar[\n]").exit(0)
     
 @check50.check(compiles)
 def test5():
